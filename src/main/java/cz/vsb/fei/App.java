f0fcdc489a6835e0940fcee322cbd70b2625b855
prezmyl <prezmyl@gmail.com>
2025-03-09 17:58:16 +0100

    first commit

---
+++ src/main/java/cz/vsb/fei/App.java
@@ -0,0 +1,16 @@
+package cz.vsb.fei;
+
+import lombok.extern.log4j.Log4j2;
+
+/**
+ *  Class <b>App</b> - main class
+ *  @author     Java I
+ */
+@Log4j2
+public class App {
+
+	public static void main(String[] args) {
+		log.info("Launching Java application.");
+	}
+	
+}
